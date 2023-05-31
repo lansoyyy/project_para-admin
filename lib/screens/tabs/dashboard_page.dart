6fc77b5269e643bff1cc35c27791ddcdcc7ed397
Lans12345 <olanalans12345@gmail.com>
2023-05-31 11:09:34 +0800

    added data on dashboard

--- lib/screens/tabs/dashboard_page.dart
+++ lib/screens/tabs/dashboard_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart' as pie;
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 import '../../widgets/text_widget.dart';
@@ -15,6 +16,7 @@ class _DashboardPageState extends State<DashboardPage> {
   @override
   void initState() {
     getTotalFare();
+    getBookings();
     super.initState();
   }
 
@@ -34,6 +36,10 @@ class _DashboardPageState extends State<DashboardPage> {
   double saturday = 0;
   double sunday = 0;
 
+  double rides = 0;
+  double cancelled = 0;
+  double pendings = 0;
+
   @override
   Widget build(BuildContext context) {
     final List<ChartData> chartData = [
@@ -48,33 +54,72 @@ class _DashboardPageState extends State<DashboardPage> {
     return Scaffold(
       body: hasLoaded
           ? Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(30, 10, 30, 20),
-                    child: Column(
-                      children: [
-                        TextRegular(
-                            text: 'Number of bookings per week',
-                            fontSize: 18,
-                            color: Colors.black),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        SfCartesianChart(
-                            primaryXAxis: CategoryAxis(),
-                            series: <ChartSeries<ChartData, String>>[
-                              // Renders column chart
-                              ColumnSeries<ChartData, String>(
-                                  dataSource: chartData,
-                                  xValueMapper: (ChartData data, _) => data.x,
-                                  yValueMapper: (ChartData data, _) => data.y)
-                            ]),
-                      ],
-                    ),
-                  )
-                ],
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(30, 10, 30, 20),
+                      child: Column(
+                        children: [
+                          TextRegular(
+                              text: 'Number of bookings per week',
+                              fontSize: 18,
+                              color: Colors.black),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          SfCartesianChart(
+                              primaryXAxis: CategoryAxis(),
+                              series: <ChartSeries<ChartData, String>>[
+                                // Renders column chart
+                                ColumnSeries<ChartData, String>(
+                                    dataSource: chartData,
+                                    xValueMapper: (ChartData data, _) => data.x,
+                                    yValueMapper: (ChartData data, _) => data.y)
+                              ]),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          const Divider(
+                            thickness: 2,
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          TextRegular(
+                              text: 'Bookings Analytics',
+                              fontSize: 18,
+                              color: Colors.black),
+                          SizedBox(
+                            height: 300,
+                            width: 1000,
+                            child: Container(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 16),
+                              child: pie.PieChart(
+                                legendOptions: const pie.LegendOptions(
+                                    legendPosition: pie.LegendPosition.left),
+                                dataMap: {
+                                  'Accepted': rides,
+                                  'Cancelled': cancelled,
+                                  'Pending': pendings,
+                                },
+                                chartType: pie.ChartType.disc,
+                                baseChartColor: Colors.grey[300]!,
+                                colorList: const [
+                                  Colors.blue,
+                                  Colors.red,
+                                  Colors.green,
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             )
           : const Center(
@@ -116,6 +161,30 @@ class _DashboardPageState extends State<DashboardPage> {
           }
           total += double.parse(doc['fare']);
         });
+      }
+    });
+  }
+
+  getBookings() async {
+    await FirebaseFirestore.instance
+        .collection('Bookings')
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      for (var doc in querySnapshot.docs) {
+        print(doc['status']);
+        if (doc['status'] == 'Accepted') {
+          setState(() {
+            rides += 1;
+          });
+        } else if (doc['status'] == 'Rejected') {
+          setState(() {
+            cancelled += 1;
+          });
+        } else {
+          setState(() {
+            pendings += 1;
+          });
+        }
       }
     });
 
