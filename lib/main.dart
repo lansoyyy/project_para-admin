f185659b01fa8ff3caf7837299f7c46a8d42cc16
Lans12345 <olanalans12345@gmail.com>
2023-05-31 10:47:47 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:project_para_admin/screens/home_screen.dart';
 import 'package:project_para_admin/screens/splashtohome_screen.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'project-phara',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
--- lib/screens/tabs/dashboard_page.dart
+++ lib/screens/tabs/dashboard_page.dart
@@ -1,10 +1,132 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
-class DashboardPage extends StatelessWidget {
+import '../../widgets/text_widget.dart';
+
+class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  @override
+  void initState() {
+    getTotalFare();
+    super.initState();
   }
+
+  List fares = [];
+
+  bool hasLoaded = false;
+
+  double total = 0;
+  double today = 0;
+  double average = 0;
+
+  double monday = 0;
+  double tuesday = 0;
+  double wednesday = 0;
+  double thursday = 0;
+  double friday = 0;
+  double saturday = 0;
+  double sunday = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<ChartData> chartData = [
+      ChartData('Mon', monday),
+      ChartData('Tue', tuesday),
+      ChartData('Wed', wednesday),
+      ChartData('Thu', thursday),
+      ChartData('Fri', friday),
+      ChartData('Sat', saturday),
+      ChartData('Sun', sunday),
+    ];
+    return Scaffold(
+      body: hasLoaded
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(30, 10, 30, 20),
+                    child: Column(
+                      children: [
+                        TextRegular(
+                            text: 'Number of bookings per week',
+                            fontSize: 18,
+                            color: Colors.black),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        SfCartesianChart(
+                            primaryXAxis: CategoryAxis(),
+                            series: <ChartSeries<ChartData, String>>[
+                              // Renders column chart
+                              ColumnSeries<ChartData, String>(
+                                  dataSource: chartData,
+                                  xValueMapper: (ChartData data, _) => data.x,
+                                  yValueMapper: (ChartData data, _) => data.y)
+                            ]),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          : const Center(
+              child: CircularProgressIndicator(),
+            ),
+    );
+  }
+
+  getTotalFare() async {
+    await FirebaseFirestore.instance
+        .collection('Bookings')
+        .where('status', isEqualTo: 'Accepted')
+        .get()
+        .then((QuerySnapshot querySnapshot) {
+      for (var doc in querySnapshot.docs) {
+        DateTime dateTime = doc['dateTime'].toDate();
+
+        setState(() {
+          if (dateTime.year == DateTime.now().year &&
+              dateTime.month == DateTime.now().month &&
+              dateTime.day == DateTime.now().day) {
+            today += double.parse(doc['fare']);
+          }
+
+          if (dateTime.weekday == DateTime.monday) {
+            monday += 1;
+          } else if (dateTime.weekday == DateTime.tuesday) {
+            tuesday += 1;
+          } else if (dateTime.weekday == DateTime.wednesday) {
+            wednesday += 1;
+          } else if (dateTime.weekday == DateTime.thursday) {
+            thursday += 1;
+          } else if (dateTime.weekday == DateTime.friday) {
+            friday += 1;
+          } else if (dateTime.weekday == DateTime.saturday) {
+            saturday += 1;
+          } else if (dateTime.weekday == DateTime.sunday) {
+            sunday += 1;
+          }
+          total += double.parse(doc['fare']);
+        });
+      }
+    });
+
+    setState(() {
+      hasLoaded = true;
+    });
+  }
+}
+
+class ChartData {
+  ChartData(this.x, this.y);
+  final String x;
+  final double y;
 }
