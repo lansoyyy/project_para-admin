15aa1e8bbafe67b4fb0701e8d0034a8d94ad6725
Lans12345 <olanalans12345@gmail.com>
2023-05-31 11:12:36 +0800

    wip mate page

--- lib/screens/tabs/map_page.dart
+++ lib/screens/tabs/map_page.dart
@@ -1,10 +1,32 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class MapPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapPage extends StatefulWidget {
   const MapPage({super.key});
 
   @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return GoogleMap(
+      mapType: MapType.hybrid,
+      initialCameraPosition: _kGooglePlex,
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
+    );
   }
 }
