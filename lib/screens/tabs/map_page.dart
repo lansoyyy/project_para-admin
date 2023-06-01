1a40b471c35d4edefb3da8b6d3a38d15c4e62692
Lans12345 <olanalans12345@gmail.com>
2023-06-01 08:44:54 +0800

    added markers on map page

--- lib/screens/tabs/map_page.dart
+++ lib/screens/tabs/map_page.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:project_para_admin/utils/colors.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -11,6 +14,13 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  @override
+  void initState() {
+    super.initState();
+    getAllDrivers();
+  }
+
+  var hasLoaded = false;
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
 
@@ -19,17 +29,62 @@ class _MapPageState extends State<MapPage> {
     zoom: 14.4746,
   );
 
+  GoogleMapController? mapController;
+
+  Set<Marker> markers = {};
+
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      myLocationEnabled: true,
-      myLocationButtonEnabled: true,
-      buildingsEnabled: true,
-      mapType: MapType.normal,
-      initialCameraPosition: _kGooglePlex,
-      onMapCreated: (GoogleMapController controller) {
-        _controller.complete(controller);
-      },
-    );
+    return hasLoaded
+        ? GoogleMap(
+            mapToolbarEnabled: false,
+            zoomControlsEnabled: false,
+            buildingsEnabled: true,
+            compassEnabled: true,
+            myLocationButtonEnabled: true,
+            myLocationEnabled: true,
+            markers: markers,
+            mapType: MapType.normal,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+
+              setState(() {
+                mapController = controller;
+              });
+            },
+          )
+        : const Center(
+            child: SpinKitPulse(
+              color: grey,
+            ),
+          );
+  }
+
+  getAllDrivers() async {
+    FirebaseFirestore.instance
+        .collection('Drivers')
+        .get()
+        .then((QuerySnapshot querySnapshot) async {
+      for (var doc in querySnapshot.docs) {
+        setState(() {
+          markers.add(
+            Marker(
+              infoWindow: InfoWindow(
+                title: doc['name'],
+                snippet: doc['isActive'] == true ? 'Active' : 'Inactive',
+              ),
+              markerId: MarkerId(doc['name']),
+              position: LatLng(doc['location']['lat'], doc['location']['long']),
+              icon: BitmapDescriptor.defaultMarker,
+            ),
+          );
+        });
+      }
+    });
+
+    setState(() {
+      hasLoaded = true;
+    });
   }
 }
